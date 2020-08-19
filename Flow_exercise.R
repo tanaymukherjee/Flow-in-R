98ef349a298f2de7c675b6beb8d0195d4209ef8f
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-08-19 19:18:05 -0400

    Add files via upload

---
+++ Flow_exercise.R
@@ -0,0 +1,8 @@
+# Install the library
+devtools::install_github("moodymudskipper/flow")
+
+# Call the package
+library(flow)
+
+# Check how it appears for a pre defined function
+flow_view(median.default)
